a72a94cb97b05142bfaca49511d2eff418a698ea
dsd <kyleighizatt@gmail.com>
2018-07-11 11:51:28 -0600

    created content component for story

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Input from './input';
+import Content from './content';
 
 class Card extends Component {
 
@@ -30,7 +31,6 @@ class Card extends Component {
 
     handleInputChange(event){
         this.setState({[event.target.name]: event.target.value})
- console.log(this.state);
     }
     render(){
 
@@ -61,6 +61,7 @@ class Card extends Component {
            {
                 inputData.map(data => Input((data), this.handleInputChange))
             }
+            <Content data={this.state}/>
 
             </div>
         )
